eb5113d4a375c3f8f4c8f4d45e53020e9727c9d0
Pablo Labarta <plabartalarrauri@gmail.com>
2024-01-12 17:57:01 +0000

    building the first componente Business

---
+++ src/components/Business.js
@@ -0,0 +1,29 @@
+import React from "react";
+import '.Business.css';
+
+const Business = ({ business }) => {
+    const business = { imageSrc, name, address, city, state, zipCode, category, rating, reviewCount };
+  
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src={imageSrc} alt={name} />
+        </div>
+        <h2>{name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{address}</p>
+            <p>{city}</p>
+            <p>{`${state} ${zipCode}`}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{category}</h3>
+            <h3 className="rating">{`${rating} stars`}</h3>
+            <p>{`${reviewCount} reviews`}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default Business;
