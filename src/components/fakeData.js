2a45e4146cdbe51297940e63b4377292e89cd9df
Pablo Labarta <plabartalarrauri@gmail.com>
2024-01-12 18:02:44 +0000

    creating fake data previous to the API

---
+++ src/components/fakeData.js
@@ -0,0 +1,11 @@
+const fakeDataBusiness = {
+    imageSrc: 'https://m.media-amazon.com/images/I/81+wWNgrPyL._AC_SX679_.jpg',
+    name: 'El Pikoteo',
+    address: 'Glenesky Square 31',
+    city: 'Dublin',
+    state: 'D15',
+    zipCode: 'D15N22F',
+    category: 'Spanish',
+    rating: 4.7,
+    reviewCount: 67,
+  };
